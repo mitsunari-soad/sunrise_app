b2ecd805bdec58d20eeb33cfd499dea21946cfa8
mitsunari-soad <hengpingshitian292@gmail.com>
2021-01-16 16:48:13 +0900

    写真投稿機能実装

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,5 +3,12 @@ class Photo < ApplicationRecord
   belongs_to :user
   belongs_to :prefecture
   has_one_attached :image
+
+
+  
+  validates :introduction, presence: true
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
   validates :prefecture_id, numericality: { other_than: 1 }
 end
